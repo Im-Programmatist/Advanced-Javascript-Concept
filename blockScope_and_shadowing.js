1a16fe48e56064ae8eb82d29f7b22448bd0c5050
Chetan Korde <chetan.korde@maximess.com>
2022-10-16 15:19:04 +0530

    blockscope and shodowing changes

--- blockScope_and_shadowing.js
+++ blockScope_and_shadowing.js
@@ -6,9 +6,10 @@ Block Scope and Shadowing</title>
     Example, if(condition) expects any statement after if() but we can put block there and run it as single statement as js required by syntax
     if(true) - if we put this only then it gives syntax error as next statement is not their, if(cond){console.log("inside iff"); var a =10; cl(a)}
     if you have same name variable outside of block then it shadowing the var variable.
-    incase of let variable it not get shadow , block variable store in block scope(also can see in insepect sourse debugger scope )
+    incase of let variable it not get shadow , block variable store in block scope(also can see in insepect source debugger scope )
     we can shadow var variable in global scope in to block scope - 
-    let a = 10; { var a = 100; } - this occures error, BUT var a = 10; { let a = 100; } - this wont get any error but  because block scope also follow lexical scope (hierarchy) and let variable can not redeclare 
+    let a = 10; { var a = 100; } - this occures error, BUT var a = 10; { let a = 100; } - this wont get any error 
+    because block scope also follow lexical scope (hierarchy) and let variable can not redeclare 
 */
     var d = 4000; // this value get shadow by block var d  
     let e = 5000; // this will not get shadow by block let e variable 
@@ -31,7 +32,7 @@ Block Scope and Shadowing</title>
         }
         //shadowing testing 
         console.warn('shadowing testing '); 
-        console.log('outside block d : ', d); //40 - must be 4000 but shadow by d present by d variable present in block 
+        console.log('outside block d : ', d); //40 - must be 4000 but shadow by d variable present in block 
         //both block variable var a and global  variable a reffers same meory block in global execution context
         console.log('outside block e : ', e); // Not shadow by e present in block variable. 
         //stored in different memory than block e(block e will be in block scope memory and global let b variable will store in script memory )
