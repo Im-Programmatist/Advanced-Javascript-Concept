57d217f3b6a8eae8a04a01e3f9e8b64a5878ce2d
chetan korde <chetannkorde@gmail.com>
2022-11-15 16:04:22 +0530

    change in splice

--- Slice&SpliceOnArray.js
+++ Slice&SpliceOnArray.js
@@ -10,7 +10,10 @@ var arrSplice = ["hello", "world", "how", "are", "you", "!"];
 //Splice,return separate array from given index(and up to which index) of parent array  
 //It does alter parent array, it remove element from given index and return that removed item as array
 //We can pass one argument to splice as well
+//first argument as index fromwhich need to remove and 
+//second argument how many item to remove 
 console.log('Splice with 1 argument ---> ',arrSplice.splice(4));//It will count 4th index and from that index remove all next item from array
+console.log('Splice with 1 argument ---> ',arrSplice.splice(3,1));
 console.log('item from 4th index removed by splice and remaining parent array - ', arrSplice);
-console.log(`Splice is of array ${arrSplice} is \t ---> ${arrSplice.splice(1,3)}`);//here 3 is outer limit index 3 will not get count in operation
+console.log(`Splice is of array ${arrSplice} is \t ---> ${arrSplice.splice(1,2)}`);//here 2 isno of item to remove from 1 index 
 console.log('Removed item from index 1,3 and remaining array is -',arrSplice);
