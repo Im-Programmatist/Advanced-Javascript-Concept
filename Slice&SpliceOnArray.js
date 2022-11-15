c26e4b17d1ada1dbdd3171fded7c20eb42a0f419
chetan korde <chetannkorde@gmail.com>
2022-11-15 10:26:51 +0530

    Added new concept of slice and splice on array

---
+++ Slice&SpliceOnArray.js
@@ -0,0 +1,16 @@
+var arrSlice = ["hello", "world", "how", "are", "you", "!"];
+//Slice,return separate array from given index(and up to which index) of parent array  
+//It does not alter parent array
+//We can pass one argument to slice as well
+console.log('Slice with 1 argument ---> ',arrSlice.slice(4));//It will count 4th index and from that all the next item will return in resulting array
+console.log(`Slice is of array ${arrSlice} is \t ---> ${arrSlice.slice(1,3)}`);//here 3 is outer limit index 3 will not get count in operation
+console.log(arrSlice);
+
+var arrSplice = ["hello", "world", "how", "are", "you", "!"];
+//Splice,return separate array from given index(and up to which index) of parent array  
+//It does alter parent array, it remove element from given index and return that removed item as array
+//We can pass one argument to splice as well
+console.log('Splice with 1 argument ---> ',arrSplice.splice(4));//It will count 4th index and from that index remove all next item from array
+console.log('item from 4th index removed by splice and remaining parent array - ', arrSplice);
+console.log(`Splice is of array ${arrSplice} is \t ---> ${arrSplice.splice(1,3)}`);//here 3 is outer limit index 3 will not get count in operation
+console.log('Removed item from index 1,3 and remaining array is -',arrSplice);
